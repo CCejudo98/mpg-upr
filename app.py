c3c158f8e5cf11d58942e9522756e7d92f1a9715
CCejudo98 <94028522+CCejudo98@users.noreply.github.com>
2026-05-25 23:19:13 -0600

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ st.title("Metric & Power Group ⚡ UPR Input")
 st.write("---")
 
 # 2. Vector de Conexión Absoluto (Pega aquí tu cadena de Neon)
-DB_URL =psql -h pg.neon.tech
+DB_URL = "postgresql://alexcejudo98:ep_dark_sound_p5p4wzrm@ep-dark-sound-a5x836m4.us-east-2.aws.neon.tech/neondb?sslmode=require"
 def query_execute(query, params=(), fetch=False):
     with psycopg2.connect(DB_URL) as conn:
         with conn.cursor() as cur:
